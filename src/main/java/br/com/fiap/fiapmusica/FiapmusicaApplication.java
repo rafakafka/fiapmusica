c9858378cd11642c74f8f3f582c86427fe8e7b0c
Rafael Martins <rafinhakafka@gmail.com>
2021-03-15 03:48:19 -0300

    first commit

---
+++ src/main/java/br/com/fiap/fiapmusica/FiapmusicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapmusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapmusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapmusicaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dto/MusicaDTO.java
@@ -0,0 +1,35 @@
+package dto;
+
+public class MusicaDTO {
+	
+	private int id;
+	private String titulo;
+	private int duracao;
+	private String autor;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getDuracao() {
+		return duracao;
+	}
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+}
